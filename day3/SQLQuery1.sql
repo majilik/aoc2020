a8afa59575952004e6598931bd336e43c124babf
David <majilik@live.se>
2020-12-05 17:46:21 +0100

    Day 3 **

--- day3/SQLQuery1.sql
+++ day3/SQLQuery1.sql
@@ -1,3 +1,32 @@
+-- Create function
+CREATE TYPE SlopeTable AS TABLE (
+	RowNumber INT,
+	MapRow NVARCHAR(MAX)
+)
+GO
+
+CREATE FUNCTION udfSlopeTreeCount(
+	@Slope AS SlopeTable READONLY,
+	@Tree NVARCHAR(1),
+	@RightSteps INT,
+	@DownSteps INT
+)
+RETURNS TABLE
+AS
+RETURN
+	SELECT COUNT(*) AS TreeCount
+	FROM (
+		SELECT 
+			RowNumber,
+			MapRow,
+			((((ROW_NUMBER() OVER (ORDER BY RowNumber)) - 1) * @RightSteps) % (LEN(MapRow))) + 1 AS Position
+		FROM @Slope
+		WHERE @DownSteps = 1 OR RowNumber % (@DownSteps) = 1
+	) SteppedRows
+	WHERE CHARINDEX(@Tree, MapRow, Position) = Position -- Tree exists at position
+	AND RowNumber > 1
+GO
+
 -- Input handling
 DECLARE @Input AS TABLE (
 	RowNumber INT IDENTITY,
@@ -11,20 +40,33 @@ FROM OPENROWSET (
 		FORMATFILE = 'C:\Users\David\Documents\aoc\aoc2020\day3\bulk_format.xml'  
 ) AS b;
 
+DECLARE @InputAsSlopeTable SlopeTable
+INSERT INTO @InputAsSlopeTable (RowNumber, MapRow)
+SELECT RowNumber, MapRow
+FROM @Input
+
+-- Queries
 -- Part 1
-DECLARE @Tree NVARCHAR(1) = '#'
-DECLARE @RightSteps INT = 3
-DECLARE @DownSteps INT = 1
+SELECT *
+FROM udfSlopeTreeCount(@InputAsSlopeTable, '#', 3, 1)
 
-SELECT COUNT(*) AS TreeCount
-FROM (
-	SELECT 
-		RowNumber,
-		MapRow,
-		((((ROW_NUMBER() OVER (ORDER BY RowNumber)) - 1) * @RightSteps) % (LEN(MapRow))) + 1 AS Position
-	FROM @Input
-	WHERE RowNumber % (@DownSteps) = 0
-) SteppedRows
-WHERE CHARINDEX(@Tree, MapRow, Position) = Position -- Tree exists at position
-AND RowNumber > 1
+-- Part 2
 
+;WITH Slopes AS (
+	SELECT 1 AS 'R', 1 AS 'D'
+	UNION
+	SELECT 3 AS 'R', 1 AS 'D'
+	UNION
+	SELECT 5 AS 'R', 1 AS 'D'
+	UNION
+	SELECT 7 AS 'R', 1 AS 'D'
+	UNION
+	SELECT 1 AS 'R', 2 AS 'D'
+)
+SELECT EXP(SUM(LOG(result.TreeCount))) AS 'MultipliedTreeCount'
+FROM Slopes s
+CROSS APPLY udfSlopeTreeCount(@InputAsSlopeTable, '#', s.R, s.D) result
+
+-- Cleanup
+DROP FUNCTION udfSlopeTreeCount
+DROP TYPE SlopeTable
