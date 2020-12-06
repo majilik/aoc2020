4173bb0727941d8f0beae9c47d5c2150baf82788
David <majilik@live.se>
2020-12-06 16:52:33 +0100

    Day 4 **

--- day4/SQLQuery1.sql
+++ day4/SQLQuery1.sql
@@ -1,3 +1,22 @@
+-- Functions
+CREATE FUNCTION udfGetNumeric(@Expression NVARCHAR(MAX))
+RETURNS INT AS 
+BEGIN 
+	DECLARE @Numeric INT
+	SELECT @Numeric = CAST(LEFT(@Expression, PATINDEX('%[0-9][^0-9]%', @Expression)) AS INT)
+	RETURN @Numeric
+END
+GO
+
+CREATE FUNCTION udfGetUnit(@Expression NVARCHAR(MAX))
+RETURNS NVARCHAR(MAX) AS 
+BEGIN 
+	DECLARE @Unit NVARCHAR(MAX)
+	SELECT @Unit = RIGHT(@Expression, LEN(@Expression) - PATINDEX('%[0-9][^0-9]%', @Expression))
+	RETURN @Unit
+END
+GO
+
 -- Input handling
 DECLARE @Input AS TABLE (
 	RowNumber INT IDENTITY,
@@ -68,3 +87,33 @@ AND hgt IS NOT NULL
 AND hcl IS NOT NULL
 AND ecl IS NOT NULL
 AND pid IS NOT NULL
+
+-- Part 2
+;WITH RequiredFields AS (
+	SELECT *
+	FROM @PassportData
+	WHERE byr IS NOT NULL
+	AND iyr IS NOT NULL
+	AND eyr IS NOT NULL
+	AND hgt IS NOT NULL
+	AND hcl IS NOT NULL
+	AND ecl IS NOT NULL
+	AND pid IS NOT NULL
+)
+	
+SELECT COUNT(*)
+FROM RequiredFields
+WHERE byr BETWEEN 1920 AND 2002
+AND iyr BETWEEN 2010 AND 2020
+AND eyr BETWEEN 2020 AND 2030
+AND (
+	(dbo.udfGetUnit(hgt) = 'in' AND dbo.udfGetNumeric(hgt) BETWEEN 59 AND 76) 
+	OR
+	(dbo.udfGetUnit(hgt) = 'cm' AND dbo.udfGetNumeric(hgt) BETWEEN 150 AND 193) 
+)
+AND hcl LIKE '#[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]'
+AND ecl IN ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth')
+AND pid LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'
+
+DROP FUNCTION udfGetNumeric
+DROP FUNCTION udfGetUnit
