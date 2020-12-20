7a953513ddb46c4eb4939cf50364406df56307a1
David <majilik@live.se>
2020-12-20 13:28:50 +0100

    Day 8 **

--- day8/SQLQuery1.sql
+++ day8/SQLQuery1.sql
@@ -1,4 +1,72 @@
 -- Functions
+CREATE TYPE udtProgram AS TABLE (
+	LineNumber INT,
+	Operation NVARCHAR(3),
+	Argument INT
+)
+GO
+CREATE FUNCTION udfRunProgram(
+	@Program AS udtProgram READONLY,
+	@LineNumberOverride AS INT,
+	@OperationOverride AS NVARCHAR(3)
+)
+RETURNS @ReturnValue TABLE (
+	RanUntilEnd BIT,
+	Accumulator INT
+)
+AS BEGIN
+	DECLARE @PC INT = 1
+	DECLARE @Acc INT = 0
+	DECLARE @Op NVARCHAR(3)
+	DECLARE @Arg INT
+	DECLARE @MaxPC INT
+	SELECT @MaxPC = MAX(LineNumber)
+	FROM @Program
+	DECLARE @VisitedLines AS TABLE (
+		LineNumber INT
+	)
+
+	WHILE (@PC <= @MaxPC)
+	BEGIN
+		SELECT @Op = Operation, @Arg = Argument
+		FROM @Program
+		WHERE LineNumber = @PC
+
+		IF EXISTS(SELECT * FROM @VisitedLines WHERE LineNumber = @PC)
+		BEGIN
+			BREAK
+		END
+
+		INSERT INTO @VisitedLines
+		VALUES (@PC)
+
+		IF @PC = @LineNumberOverride
+		BEGIN
+			SET @Op = @OperationOverride
+		END
+	
+		IF @Op = 'acc'
+		BEGIN
+			SET @Acc = @Acc + @Arg
+		END
+		ELSE IF @Op = 'jmp'
+		BEGIN
+			SET @PC = @PC + @Arg
+			CONTINUE
+		END
+	
+		SET @PC = @PC + 1	
+	END
+	
+	INSERT INTO @ReturnValue(RanUntilEnd, Accumulator)
+	VALUES (
+		CASE WHEN @PC >= @MaxPC THEN 1 ELSE 0 END, 
+		@Acc
+	)
+
+	RETURN
+END
+GO
 
 -- Input handling
 DECLARE @Input AS TABLE (
@@ -14,53 +82,37 @@ FROM OPENROWSET (
 		FORMATFILE = 'C:\Users\David\Documents\aoc\aoc2020\day8\bulk_format.xml'  
 ) AS b;
 
+DECLARE @Program AS dbo.udtProgram
+INSERT INTO @Program
+SELECT *
+FROM @Input
+
 -- Queries
 -- Part 1
-DECLARE @PC INT = 1
-DECLARE @Acc INT = 0
-DECLARE @Op NVARCHAR(3)
-DECLARE @Arg INT
-DECLARE @MaxPC INT
-SELECT @MaxPC = MAX(LineNumber)
-FROM @Input
-DECLARE @VisitedLines AS TABLE (
-	LineNumber INT
+SELECT *
+FROM dbo.udfRunProgram(@Program, DEFAULT, DEFAULT)
+
+-- Part 2
+-- Brute force JMP to NOP
+;WITH ProgramRuns AS (
+	SELECT x.RanUntilEnd, x.Accumulator
+	FROM @Program p
+	CROSS APPLY dbo.udfRunProgram(@Program, p.LineNumber, 'nop') x
+	WHERE Operation = 'jmp'
+
+	UNION ALL
+
+	-- Brute force NOP to JMP
+	SELECT x.RanUntilEnd, x.Accumulator
+	FROM @Program p
+	CROSS APPLY dbo.udfRunProgram(@Program, p.LineNumber, 'jmp') x
+	WHERE Operation = 'nop'
 )
 
-WHILE (@PC <= @MaxPC)
-BEGIN
-	SELECT @Op = Operation, @Arg = Argument
-	FROM @Input
-	WHERE LineNumber = @PC
+SELECT *
+FROM ProgramRuns
+WHERE RanUntilEnd = 1
 
-	PRINT 'PC: ' + CAST(@PC AS NVARCHAR(MAX)) + ' | Acc: ' + CAST(@Acc AS NVARCHAR(MAX)) + ' | Op: ' + @Op + ' | Arg: ' +  CAST(@Arg AS NVARCHAR(MAX)) 
-
-	IF EXISTS(SELECT * FROM @VisitedLines WHERE LineNumber = @PC)
-	BEGIN
-		PRINT 'Revisiting already visited line, breaking program.'
-		BREAK
-	END
-
-	INSERT INTO @VisitedLines
-	VALUES (@PC)
-	
-	IF @Op = 'nop'
-	BEGIN
-		PRINT 'Program nop: Acc = ' + CAST(@Acc AS NVARCHAR(MAX))
-	END
-	ELSE IF @Op = 'acc'
-	BEGIN
-		SET @Acc = @Acc + @Arg
-		PRINT 'Program accumulation: Acc = ' + CAST(@Acc AS NVARCHAR(MAX))
-	END
-	ELSE IF @Op = 'jmp'
-	BEGIN
-		SET @PC = @PC + @Arg
-		PRINT 'Program jump: PC = ' + CAST(@PC AS NVARCHAR(MAX))
-		CONTINUE
-	END
-	
-	SET @PC = @PC + 1	
-END
-
-PRINT 'Program ended, Acc = ' + CAST(@Acc AS NVARCHAR(MAX))
+-- Cleanup
+DROP FUNCTION dbo.udfRunProgram
+DROP TYPE dbo.udtProgram
